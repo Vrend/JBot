31b0f8087b72f4e1d85cf3ea4a1687da1d44d245
John Broderick <johnbroderick12@gmail.com>
2017-01-07 19:54:22 -0500

    finallly updated
    fuck git
    
    fuck maven
    
    fuck vcs
    
    fuck intellij
    
    gah

---
+++ src/Commands/play.java
@@ -0,0 +1,73 @@
+package Commands;
+
+import Util.AudioPlayerSendHandler;
+import Util.BadCommandException;
+import Util.Command;
+import Util.TrackSchedule;
+import Util.AudioHolder;
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.managers.AudioManager;
+
+public class play implements Command
+{
+    @Override
+    public void run(MessageReceivedEvent e, String[] args) throws BadCommandException
+    {
+        if(e.getGuild().getAudioManager().getConnectedChannel() == null)
+        {
+            Command s = new summon();
+            s.run(e, args);
+        }
+
+        if(args.length != 2)
+        {
+            throw new BadCommandException("Malformed Command Request: Improper Arguments");
+        }
+
+        AudioPlayerManager playerManager = AudioHolder.getManager();
+
+        AudioPlayer player = playerManager.createPlayer();
+
+        TrackSchedule ts = new TrackSchedule(player);
+        player.addListener(ts);
+
+        playerManager.loadItem(args[1], new AudioLoadResultHandler() {
+                    @Override
+                    public void trackLoaded(AudioTrack track) {
+                        ts.queue(track);
+                    }
+
+                    @Override
+                    public void playlistLoaded(AudioPlaylist playlist) {
+                        for (AudioTrack track : playlist.getTracks()) {
+                            ts.queue(track);
+                        }
+                    }
+
+                    @Override
+                    public void noMatches() {
+                        // Notify the user that we've got nothing
+                    }
+
+                    @Override
+                    public void loadFailed(FriendlyException throwable) {
+                        // Notify the user that everything exploded
+                    }
+                });
+        AudioManager am = e.getGuild().getAudioManager();
+
+        am.setSendingHandler(new AudioPlayerSendHandler(player));
+    }
+
+    @Override
+    public int getPermLevel()
+    {
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import Util.AudioHolder;
 import Listeners.messageListener;
 import Util.IO;
 import net.dv8tion.jda.core.AccountType;
@@ -6,11 +7,14 @@ import net.dv8tion.jda.core.JDABuilder;
 
 public class Main
 {
+
     public static void main(String[] args)
     {
         String botapi;
 
         botapi = IO.getKey("Keys/botapi.key");
+
+        AudioHolder.init();
 
         try
         {
@@ -22,5 +26,6 @@ public class Main
         }
 
     }
+
 
 }
