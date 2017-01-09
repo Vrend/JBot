3f45740b5b316a6736424550c3e882530ac13dc4
John Broderick <johnbroderick12@gmail.com>
2017-01-08 21:48:07 -0500

    added a prune command and harambe meme command

---
+++ src/Admin/prune.java
@@ -0,0 +1,40 @@
+package Admin;
+
+import Util.BadCommandException;
+import Util.Command;
+import net.dv8tion.jda.core.MessageHistory;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class prune implements Command
+{
+    @Override
+    public void run(MessageReceivedEvent e, String[] args) throws BadCommandException
+    {
+        if(args.length != 2)
+        {
+            throw new BadCommandException("Malformed Command Request: Improper Arguments");
+        }
+
+        try
+        {
+            MessageHistory mh = e.getChannel().getHistory();
+            List<Message> messages = mh.retrievePast(Integer.parseInt(args[1])).block();
+            e.getTextChannel().deleteMessages(messages).queue();
+        }
+        catch(Exception e1)
+        {
+            throw new BadCommandException("Malformed Command Request: Improper Arguments");
+        }
+
+
+    }
+
+    @Override
+    public int getPermLevel()
+    {
+        return 1;
+    }
+}
---
+++ src/Commands/harambe.java
@@ -0,0 +1,23 @@
+package Commands;
+
+import Util.BadCommandException;
+import Util.Command;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+
+public class harambe implements Command
+{
+    @Override
+    public void run(MessageReceivedEvent e, String[] args) throws BadCommandException
+    {
+        Command play = new play();
+        String[] arguments = {"play", "https://www.youtube.com/watch?v=OJw3MmL-Omk"};
+        play.run(e, arguments);
+        e.getChannel().sendMessage(e.getMember().getAsMention()+" `we will never forget you, Harambe.`").queue();
+    }
+
+    @Override
+    public int getPermLevel()
+    {
+        return 1;
+    }
+}
