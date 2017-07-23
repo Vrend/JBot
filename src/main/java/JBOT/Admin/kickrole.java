881bb03e953648d6c3b11b56980ccca740ea414e
John Broderick <johnbroderick12@gmail.com>
2017-07-23 01:26:33 -0400

    cleaning up

--- src/main/java/JBOT/Admin/kickrole.java
+++ src/main/java/JBOT/Admin/kickrole.java
@@ -6,7 +6,6 @@ import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Role;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.managers.GuildController;
-
 import java.util.List;
 
 public class kickrole implements Command
